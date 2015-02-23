456cb072f13a461194a2ee0f668387489b37a0f5
root <root@longc.wang>
2015-02-23 15:49:14 +0800

    create db

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from app import myapp, db
 from models import ClassRoom
 import view
 
+db.create_tables([ClassRoom], safe=True)
 if __name__ == '__main__':
     db.create_tables([ClassRoom], safe=True)
     myapp.run('0.0.0.0')
