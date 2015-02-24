d89eff20ab22d150cb25ceb86bc543a30d8413e6
Chenlong <hustcl1024@gmail.com>
2015-02-24 11:00:38 +0800

    使用config

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 # coding: utf-8
 
 import os
+import ConfigParser
 
 from flask import Flask
-from peewee import SqliteDatabase, MySQLDatabase
+from peewee import MySQLDatabase
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-# DATABASE = os.path.join(APP_ROOT, 'classroom.db')
+CONFIG_FILE = os.path.join(APP_ROOT, 'server_config/config.ini')
 DEBUG = True
 
 myapp = Flask(__name__)
 myapp.config.from_object(__name__)
-# db = SqliteDatabase(myapp.config['DATABASE'], threadlocals=True)
-db = MySQLDatabase('hust.longc.wang', threadlocals=True, host='localhost', user='root', password='123456')
+
+cf = ConfigParser.ConfigParser()
+cf.read(CONFIG_FILE)
+
+db_host = cf.get('mysql', 'host')
+db_user = cf.get('mysql', 'user')
+db_password = cf.get('mysql', 'password')
+db_database = cf.get('mysql', 'database')
+
+db = MySQLDatabase(db_database, threadlocals=True, host=db_host, user=db_user, password=db_password)
